6785901f919729ef30809062f2ad89080c52903a
iSaul18 <porrasemiliosaul@gmail.com>
2022-10-06 12:21:33 -0500

    codeWars 3

--- codeWars2/codeWars2.js
+++ codeWars2/codeWars2.js
@@ -1,16 +1,28 @@
 function points(games) {
   return games.reduce((acc, game) => {
-    if (game[0] === game[2]) return acc += 1;
+    let [myTeam, rivalTeam] = game.split(":");
+    myTeam = Number(myTeam);
+    rivalTeam = Number(rivalTeam);
 
-    let victory = game[0] > game[2] ? 3 : 0;
-    return acc += victory;
+    if (myTeam === rivalTeam) return (acc += 1);
+
+    let victory = myTeam > rivalTeam ? 3 : 0;
+    return (acc += victory);
   }, 0);
 }
-
 /*
 console.log(points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"])); //30
 console.log(points(["1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"])); //10
 console.log(points(["0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"])); //0
 console.log(points(["1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4"])); //15
 console.log(points(["1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4"])); //12
+*/
+/*
+// ?? devuelve el primer elemento que no sea null o undefined;
+//  || devuelve el primer elemento que sea true;
+//  && devuelve el primer elemento que sea false;
+
+console.log(undefined ?? false ?? 10);
+console.log(undefined && false && 10);
+console.log(undefined || false || 10);
 */
