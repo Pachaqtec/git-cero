0342852213ddbe1b570e38588dbf82a8fa899843
iSaul18 <porrasemiliosaul@gmail.com>
2022-10-06 12:45:02 -0500

    code warss 4

--- codeWars4/codeWars4.js
+++ codeWars4/codeWars4.js
@@ -1,12 +1,11 @@
 function isSortedAndHow(array) {
   const ascending = JSON.stringify([...array].sort((a, b) => (a > b ? 1 : -1)));
-    const descending = JSON.stringify([...array].sort((a, b) => (a > b ? -1 : 1)));
-    
+  const descending = JSON.stringify([...array].sort((a, b) => (a > b ? -1 : 1)));
+
   array = JSON.stringify(array);
 
   if (array === ascending || array === descending) return array === ascending ? "yes, ascending" : "yes, descending";
-    return "no";
-    
+  return "no";
 }
 /*
 console.log(isSortedAndHow([1, 2]));
