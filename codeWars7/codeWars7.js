75dbe4da124fb7efdcaf860c56761986a5c390ba
iSaul18 <porrasemiliosaul@gmail.com>
2022-10-07 10:49:03 -0500

    code Wars 7

---
+++ codeWars7/codeWars7.js
@@ -0,0 +1,32 @@
+function countSheeps(arrayOfSheep) {
+  return arrayOfSheep.filter((arr) => arr).length;
+}
+
+console.log(
+  countSheeps([
+    true,
+    true,
+    true,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    true,
+    true,
+  ])
+); //17
