318c539b5ca42ec26bd2e5d18772010e365d02eb
iSaul18 <porrasemiliosaul@gmail.com>
2022-10-21 12:26:40 -0500

    code Wars 10 -v2

--- codeWars10/codeWars10.js
+++ codeWars10/codeWars10.js
@@ -1,9 +1,4 @@
-function betterThanAverage(classPoints, yourPoints) {
-  const promNotas = classPoints.reduce((acc, nota) => (acc += nota / classPoints.length), 0);
-
-  console.log(promNotas);
-
-  return promNotas < yourPoints;
-}
+const betterThanAverage = (classPoints, yourPoints) =>
+  classPoints.reduce((acc, nota) => (acc += nota / classPoints.length), 0) < yourPoints;
 
 console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75));
